7199d8f9c8ab024767d36f3f409e95f923c23799
Benjamin Wagner <bwagner@vapita.de>
2022-08-23 05:42:55 +0200

    fix bug

--- templates/base.html.php
+++ templates/base.html.php
@@ -11,7 +11,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="<?= $meta->get('description') ?>"
+        <meta name="description" content="<?= $meta->get('description') ?>">
         <meta name="author" content="<?= $meta->get('author') ?>">
 
         <?php if ($this->section('css')): ?>
