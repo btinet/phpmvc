c09712b0c6bcc9825035bdc81015d570913ae8ce
Benjamin Wagner <bwagner@vapita.de>
2022-10-19 11:46:15 +0200

    update lesson 1

--- config/lektion_01.sql
+++ config/lektion_01.sql
@@ -30,3 +30,28 @@ select name as Land, einwohner as Einwohner from cia where name ="Deutschland" o
 
 select name as Land, Einwohner, einwohner * 1E-06 as 'Einwohner in Millionen' from cia where region ="Südamerika";
 
+# Übung 7
+# Gib die Namen und das pro Kopf-Jahreseinkommen der Länder mit mehr als 200 Millionen Einwohnern aus.
+
+select name as Land, bip / einwohner as 'pro Kopf-Jahreseinkommen' from cia where einwohner > 2*1E08;
+
+# Übung 8
+# Wie vorherige Aufgabe, allerdings sollen die Ergebnisse absteigend nach dem pro Kopf Jahreseinkommen sortiert werden.
+
+select name as Land, bip / einwohner as 'pro Kopf-Jahreseinkommen' from cia where einwohner > 2*1E08 order by bip / einwohner desc;
+
+# Übung 9
+# In welchen Ländernamen kommt das Wort 'Vereinigte' vor?
+
+select name as Land from cia where name like '%Vereinigte%';
+
+# Übung 10
+# Lasse Dir in alphabetischer Reihenfolge alle verschiedenen Regionen anzeigen.
+
+select region as Region from cia group by region;
+select distinct region as Region from cia region;
+
+# Übung 11
+# Zeige alle Länder, bei denen das Bruttoinlandsprodukt unbekannt ist.
+
+select name as Land from cia where bip is null;
